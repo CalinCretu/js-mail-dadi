2654e408874f9d5941f65528eb4a9309d9587e55
CalinCretu <calincretu1412@gmail.com>
2023-10-09 19:56:51 +0200

    ADD - dadi_html_css_js

--- dadi/js/app.js
+++ dadi/js/app.js
@@ -1,1 +1,34 @@
-console.log('dadi')
+console.log('dadi');
+
+let winnerAi = 'Hai perso!';
+
+let winnerUser = 'Hai vinto!';
+
+let draw = 'Pareggio!'
+
+let aiResult = document.getElementById('box');
+
+let playBtnDOMElement = document.getElementById('play-btn');
+
+let resultAi = document.querySelector('.number-ai');
+
+let resultUser = document.querySelector('.number-user');
+
+playBtnDOMElement.addEventListener('click', function () {
+  let aiNumber = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+  console.log(aiNumber);
+  resultAi.innerHTML = aiNumber;
+
+  let userNumber = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+  console.log(userNumber);
+  resultUser.innerHTML = userNumber;
+
+  if (aiNumber > userNumber) {
+    aiResult.innerHTML = winnerAi;
+
+  } else if (aiNumber < userNumber) {
+    aiResult.innerHTML = winnerUser;
+  } else {
+    aiResult.innerHTML = draw
+  }
+})
